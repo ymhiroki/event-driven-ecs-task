08d6fe818dd546d3a94f2c5c350c23d0478c1845
Yamazaki Hiroki <yhirokiy@amazon.co.jp>
2023-07-28 17:12:22 +0900

    add s3bucket

--- src/lib/event-driven-ecs-task-stack.ts
+++ src/lib/event-driven-ecs-task-stack.ts
@@ -3,6 +3,8 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as events from 'aws-cdk-lib/aws-events';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export interface EventDrivenEcsTaskStackProps extends cdk.StackProps {
@@ -11,6 +13,21 @@ export interface EventDrivenEcsTaskStackProps extends cdk.StackProps {
 export class EventDrivenEcsTaskStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: EventDrivenEcsTaskStackProps) {
     super(scope, id, props);
+
+    // ログ保存用のバケット
+    const logBucket = new s3.Bucket(this, 'LogBucket', {
+      autoDeleteObjects: true,
+      enforceSSL: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // タスクで生成したオブジェクトを保存するバケット
+    const bucket = new s3.Bucket(this, 'Bucket', {
+      autoDeleteObjects: true,
+      enforceSSL: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      serverAccessLogsBucket: logBucket,
+    });
 
     // タスクが動作する VPC を作成
     const vpc = new ec2.Vpc(this, 'Vpc', {
@@ -21,8 +38,15 @@ export class EventDrivenEcsTaskStack extends cdk.Stack {
       vpc,
     });
 
+    // タスクが利用する IAM ロールを作成
+    const taskRole = new iam.Role(this, 'TaskRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    });
+    bucket.grantReadWrite(taskRole);
+
     // タスクで動作するコンテナを設定
     const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDefinition', {
+      taskRole,
     });
     taskDefinition.addContainer('AppContainer', {
       image: ecs.ContainerImage.fromAsset('./app'),
@@ -41,5 +65,12 @@ export class EventDrivenEcsTaskStack extends cdk.Stack {
       cluster,
       taskDefinition,
     }));
+
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: bucket.bucketName,
+    });
+    new cdk.CfnOutput(this, 'BucketURL', {
+      value: bucket.bucketWebsiteUrl,
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { App } from 'aws-cdk-lib';
 import { EventDrivenEcsTaskStack } from './lib/event-driven-ecs-task-stack';
+// import { AwsPrototypingChecks, PDKNag } from '@aws-prototyping-sdk/pdk-nag';
+
 
 const app = new App();
+
+// const app = PDKNag.app({
+//   nagPacks: [new AwsPrototypingChecks()],
+// });
 
 new EventDrivenEcsTaskStack(app, 'event-driven-ecs-task-dev', {});
 
